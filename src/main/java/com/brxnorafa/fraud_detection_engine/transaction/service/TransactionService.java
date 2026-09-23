3f5620341b95e8dc54929719c527b22d246a0a48
brxnogoncalves <bruno.goncalves@sptech.school>
2026-09-22 23:54:19 -0300

    feat: add transaction service

---
+++ src/main/java/com/brxnorafa/fraud_detection_engine/transaction/service/TransactionService.java
@@ -0,0 +1,33 @@
+package com.brxnorafa.fraud_detection_engine.transaction.service;
+
+import com.brxnorafa.fraud_detection_engine.transaction.entity.Transaction;
+import com.brxnorafa.fraud_detection_engine.transaction.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransactionService {
+    private final TransactionRepository transactionRepository;
+
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public Transaction create(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public List<Transaction> findAll() {
+        return transactionRepository.findAll();
+    }
+
+    public Transaction findById(Long id) {
+        return transactionRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Transaction Not Found"));
+    }
+
+    public void delete(Long id) {
+        transactionRepository.deleteById(id);
+    }
+}
